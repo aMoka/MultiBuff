a70f64af472d8c12116d5f00d25b69b0ca528be4
Kristine Ku <kku4you@gmail.com>
2014-06-04 19:51:47 -0700

    Maybe I should include everything that's in the dll. >__>

---
+++ MultiBuff/MBPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Terraria;
+using TShockAPI;
+
+namespace MultiBuff
+{
+    public class MBPlayer
+    {
+        public int Index;
+        public bool isEverBuff;
+        public bool isEverDebuff;
+
+        public string name { get { return Main.player[Index].name; } }
+        public TSPlayer TSPlayer { get { return TShock.Players[Index]; } }
+
+        public MBPlayer(int index)
+        {
+            Index = index;
+        }
+    }
+}
---
+++ MultiBuff/Tools.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Timers;
+using Terraria;
+using TShockAPI;
+
+namespace MultiBuff
+{
+    public class Tools
+    {
+        public static List<MBPlayer> Players = new List<MBPlayer>();
+        public static Timer everBuffTimer = new Timer(100);
+
+        public static void initializeTimers()
+        {
+            everBuffTimer.Elapsed += new ElapsedEventHandler(eBTimer);
+        }
+
+        public static void eBTimer(object sender, ElapsedEventArgs args)
+        {
+            int count = 0;
+            foreach (MBPlayer player in Players)
+            {
+                if (player.isEverBuff)
+                {
+                    foreach (int activeBuff in player.TSPlayer.TPlayer.buffType)
+                    {
+                        player.TSPlayer.SetBuff(activeBuff, 32400);
+                        player.TSPlayer.TPlayer.AddBuff(activeBuff, 32400);
+                    }
+                    count++;
+                }
+            }
+        }
+
+        public static List<MBPlayer> GetPlayerList(string name)
+        {
+            foreach (MBPlayer player in Players)
+            {
+                if (player.name.ToLower().Contains(name.ToLower()))
+                {
+                    return new List<MBPlayer>() { player };
+                }
+            }
+            return new List<MBPlayer>();
+        }
+
+        public static MBPlayer GetPlayer(int index)
+        {
+            foreach (MBPlayer player in Players)
+                if (player.Index == index)
+                    return player;
+
+            return null;
+        }
+    }
+}
